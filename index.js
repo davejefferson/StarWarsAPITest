5d52ed3ff010826adf59be9226492ea58c320a81
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-05-26 15:58:00 +0100

    complete working button to randomise planets

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-let content;
 
-const fetchApiData =  fetch("https://swapi.co/api/planets/1/")
-.then((response) => {
-    return response.json();
- })
-    .then ((myJson) => {
-        console.log(myJson);
-        content = myJson; 
-        changeInnerHtml();
-       
-        console.log(content.name)
-    });
+
+
+let orignalNumber;
+
+const planetNumber = () => {
+    return Math.floor(Math.random() * 61 + 1);
+   
+}
+
+
+
+const fetchApiData = (event) => {
+    if (event){
+    fetch("https://swapi.co/api/planets/"+ planetNumber())
+    .then((response) => {
+        return response.json();
+    })
+        .then ((myJson) => {
+            console.log(myJson);
+            content = myJson; 
+            changeInnerHtml();
+        
+            console.log(content.name)
+        });
+    } else{}
+}
+
+
+
+
+
 
 const changeInnerHtml = () => {
     console.log("HI")
     document.getElementById("apiInfo").innerHTML = content.name;
 }
 
-
+const button = document.getElementById("button").addEventListener("click", fetchApiData) ;
